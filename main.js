d76d66e7cf252dd3872faf0c41d8762f7cd894fe
anugrah-danang <danangabiyoga26@gmail.com>
2018-03-11 21:02:10 +1300

    initial commit

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-let sliderColors = document.querySelectorAll('.slide'),
-    arrowRight = document.querySelector('#arrow-right'),
-    arrowLeft = document.querySelector('#arrow-left'),
+let sliderColors = document.querySelectorAll(".slide"),
+    arrowRight = document.querySelector("#arrow-right"),
+    arrowLeft = document.querySelector("#arrow-left"),
     current = 0;
 
 // clear all images
-    function reset (){
-    for(let i = 0; i < sliderColors.length; i++){
+    function reset () {
+    for (let i = 0; i < sliderColors.length; i++){
         sliderColors[i].style.display = "none";
     }
 }
 // initialise the slider
-    function startSlide(){
+    function startSlide() {
         reset();
         sliderColors[0].style.display = "block";
     }
 // show previous
 
-    function slideLeft(){
+    function slideLeft() {
         reset();
-        sliderColors[current - 1].style.display = "block"
+        sliderColors[current - 1].style.display = "block";
         current--;
     }
 // show next
-    function slideRight(){
+    function slideRight() {
         reset();
-        sliderColors[current + 1].style.display = "block"
+        sliderColors[current + 1].style.display = "block";
         current++;
 
 // left arrow click 
@@ -41,7 +41,7 @@ let sliderColors = document.querySelectorAll('.slide'),
         if (current === sliderColors.length - 1) {
             current = -1;
         }
-        sliderRight();
+        slideRight();
     });
 
     startSlide();
