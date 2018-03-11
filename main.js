11cf820314ef82e9ab1f12e8bba87efef2624f66
anugrah-danang <danangabiyoga26@gmail.com>
2018-03-11 21:05:24 +1300

    initial commit

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
-let sliderColors = document.querySelectorAll(".slide"),
-    arrowRight = document.querySelector("#arrow-right"),
-    arrowLeft = document.querySelector("#arrow-left"),
-    current = 0;
+let sliderImages = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
 
-// clear all images
-    function reset () {
-    for (let i = 0; i < sliderColors.length; i++){
-        sliderColors[i].style.display = "none";
-    }
+// Clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
 }
-// initialise the slider
-    function startSlide() {
-        reset();
-        sliderColors[0].style.display = "block";
-    }
-// show previous
 
-    function slideLeft() {
-        reset();
-        sliderColors[current - 1].style.display = "block";
-        current--;
-    }
-// show next
-    function slideRight() {
-        reset();
-        sliderColors[current + 1].style.display = "block";
-        current++;
+// Init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
 
-// left arrow click 
+// Show prev
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
 
-    arrowLeft.addEventListener("click", function() {
-        if (current === 0) {
-            current = sliderColors.length;
-        }
-        slideLeft();
-    });
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
 
-// right arrow click
-    arrowRight.addEventListener("click", function() {
-        if (current === sliderColors.length - 1) {
-            current = -1;
-        }
-        slideRight();
-    });
+// Left arrow click
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
 
-    startSlide();
+// Right arrow click
+arrowRight.addEventListener("click", function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
